92ad586d0033f2d735446dd5d01b585de72d4edc
Jeffyter Saraiva <jeffytersaraiva@gmail.com>
2019-12-25 20:24:52 -0300

    Removido teste

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
 require 'init.php';
-
-$Select = new \Database\Select();
-
-$Select->table = 'users';
-$Select->where = 'id = :id';
-
-$Select->setAttribute(':id', 1);
-
-var_dump($Select->get());
